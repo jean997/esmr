02935343428fe2d63d789f80a0e85f6d1f24e671
Jean Morrison <morrison.jeanv@gmail.com>
2022-11-29 17:54:38 -0500

    add post_prob_thresh option

--- R/eb_mr.R
+++ R/eb_mr.R
@@ -14,7 +14,8 @@ eb_mr <- function(beta_hat_Y, se_Y, beta_hat_X, se_X,
                   max_iter = 100,
                   seed = 123, sigma_beta = Inf,
                   tol = default_precision(c(ncol(beta_hat_X)+1, nrow(beta_hat_X))),
-                  pval_thresh =1, lfsr_thresh = 1,
+                  pval_thresh =1,
+                  post_prob_thresh = 0,
                   beta_m_init = NULL, which_beta = NULL,
                   fix_beta = FALSE,
                   beta_joint = TRUE,
@@ -24,6 +25,22 @@ eb_mr <- function(beta_hat_Y, se_Y, beta_hat_X, se_X,
   set.seed(seed)
 
   dat <- set_data(beta_hat_Y, se_Y, beta_hat_X, se_X, R)
+
+  dat$pval_thresh <- pval_thresh
+  dat$post_prob_thresh <- post_prob_thresh
+
+  if(pval_thresh < 1){
+    pval <- with(dat, 2*pnorm(-abs(Y/S)))
+    pval_min <- apply(pval[,-1,drop = FALSE], 1, min)
+    ix <- which(pval_min < pval_thresh)
+    dat <- subset_data(dat, ix)
+  }else if(post_prob_thresh > 0){
+    wpost <- get_wpost(dat$Y, dat$S, 2:p, prior_family = "point_normal")
+    wpost_max <- apply(wpost, 1, max)
+    ix <- which(wpost_max >= post_prob_thresh)
+    dat <- subset_data(dat, ix)
+  }
+
   n <- dat$n
   p <- dat$p
 
@@ -37,12 +54,6 @@ eb_mr <- function(beta_hat_Y, se_Y, beta_hat_X, se_X,
   dat$ebnm_fn <- ebnm_fn
   dat$sigma_beta <- sigma_beta
 
-  dat$pval <- with(dat, 2*pnorm(-abs(Y/S)))
-  dat$pval_min <- apply(dat$pval[,-1,drop = FALSE], 1, min)
-  dat$ix <- which(dat$pval_min < pval_thresh)
-
-  dat$lfsr_thresh <- lfsr_thresh
-  dat$pval_thresh <- pval_thresh
   dat$est_tau <- est_tau
   dat$ll <- ll
 
@@ -52,58 +63,5 @@ eb_mr <- function(beta_hat_Y, se_Y, beta_hat_X, se_X,
 }
 
 
-calc_ell <- function(Y, lbar, l2bar, fbar, f2bar, omega){
-  n <- nrow(Y)
-  p <- ncol(Y)
-  stopifnot(nrow(lbar) == n & ncol(lbar) == p)
-  stopifnot(nrow(l2bar) == n & ncol(l2bar) == p)
-  stopifnot(nrow(fbar) == p & nrow(f2bar) == p & ncol(fbar) == p & ncol(f2bar) == p)
-  #stopifnot(length(beta_m) == p-1 & length(beta_s) == p-1)
 
-
-  if("matrix" %in% class(omega)){
-    s_equal <- TRUE
-  }else{
-    stopifnot(class(omega) == "list")
-    stopifnot(length(omega) == n)
-    s_equal <- FALSE
-  }
-
-  ybar <- lbar %*% t(fbar)
-
-  varlbar <- l2bar - (lbar^2)
-  varfbar <- f2bar - (fbar^2)
-  if(s_equal){
-    part_a <- map_dbl(seq(nrow(ybar)), function(i){
-      crossprod(t(Y[i,,drop =F]), omega) %>% tcrossprod(ybar[i, , drop = F])
-    }) %>% sum()
-    part_b <- map_dbl(seq(nrow(ybar)), function(i){
-      crossprod(t(ybar[i,,drop =F]), omega) %>% tcrossprod(ybar[i, , drop = F])
-    }) %>% sum()
-    x1 <- sum(t( t(tcrossprod(lbar^2 , varfbar))*diag(omega)))
-    x2 <- sum(t( t(tcrossprod(varlbar , fbar^2))*diag(omega)))
-    x3 <- sum(t( t(tcrossprod(varlbar , varfbar))*diag(omega)))
-    part_b <- part_b + x1 + x2 + x3
-    ell <- part_a - 0.5*part_b
-  }else{
-    part_a <- map_dbl(seq(nrow(ybar)), function(i){
-      crossprod(t(Y[i,,drop =F]), omega[[i]]) %>% tcrossprod(ybar[i, , drop = F])
-    }) %>% sum()
-    part_b <- map_dbl(seq(nrow(ybar)), function(i){
-      crossprod(t(ybar[i,,drop =F]), omega[[i]]) %>% tcrossprod(ybar[i, , drop = F])
-    }) %>% sum()
-    x1 <- map_dbl(seq(nrow(ybar)), function(i){
-      sum(t(t( tcrossprod((lbar^2)[i,,drop =F], varfbar))*diag(omega[[i]])) )
-    }) %>% sum()
-    x2 <- map_dbl(seq(nrow(ybar)), function(i){
-      sum(t(t( tcrossprod(varlbar[i,,drop =F], fbar^2))*diag(omega[[i]])) )
-    }) %>% sum()
-    x3 <- map_dbl(seq(nrow(ybar)), function(i){
-     sum(t(t( tcrossprod(varlbar[i,,drop =F], varfbar))*diag(omega[[i]])) )
-    }) %>% sum()
-    part_b <- part_b + x1 + x2 + x3
-    ell <- part_a - 0.5*part_b
-  }
-  return(ell)
-}
 

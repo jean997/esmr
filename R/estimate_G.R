271b85dd169c3ec417ae792b3ad4182d0b3c070e
Jean Morrison <morrison.jeanv@gmail.com>
2023-03-01 16:07:47 -0500

    add trait scaling, try a new augmentation scheme

--- R/estimate_G.R
+++ R/estimate_G.R
@@ -11,6 +11,8 @@ estimate_G <- function(beta_hat_X, se_X, R=NULL ,
   se_X <- check_matrix(se_X, "se_X", n, p)
   Z <- beta_hat_X/se_X
 
+  trait_scale <- c(1, apply(se_X[, -1, drop = FALSE]/se_X[,1], 2, median))
+
   if(is.null(R)){
     if(type == "gfa"){
       gfit <- sumstatFactors::gfa_fit(Z_hat = Z)
@@ -35,21 +37,33 @@ estimate_G <- function(beta_hat_X, se_X, R=NULL ,
     }
   }
   if(augment){
-    cmax <- apply(abs(myG), 2, max)
-    if(any(cmax > single_trait_thresh)){
-      st_factors <- which(cmax > single_trait_thresh)
-      which_st <- apply(abs(myG[,st_factors]), 2, which.max)
-      if(!all((1:p) %in% which_st)){
-        n_add <- sum(!(1:p) %in% which_st)
-        which_add <- (1:p)[!(1:p) %in% which_st]
-        A <- matrix(0, nrow = p, ncol = n_add)
-        for(j in seq_along(which_add)) A[which_add[j],j] <- 1
-        myG <- cbind(myG, A)
-      }
-    }else{
-      myG <- cbind(myG, diag(p))
+    # cmax <- apply(abs(myG), 2, max)
+    # if(any(cmax > single_trait_thresh)){
+    #   st_factors <- which(cmax > single_trait_thresh)
+    #   which_st <- apply(abs(myG[,st_factors]), 2, which.max)
+    #   if(!all((1:p) %in% which_st)){
+    #     n_add <- sum(!(1:p) %in% which_st)
+    #     which_add <- (1:p)[!(1:p) %in% which_st]
+    #     A <- matrix(0, nrow = p, ncol = n_add)
+    #     for(j in seq_along(which_add)) A[which_add[j],j] <- 1
+    #     myG <- cbind(myG, A)
+    #   }
+    # }else{
+    #   myG <- cbind(myG, diag(p))
+    # }
+
+    # Second augment version
+    if(ncol(myG) < p){
+      n_add <- p-ncol(myG)
+      r <- rowSums(myG^2)
+      which_add <- order(r)[1:n_cols_need]
+      A <- matrix(0, nrow = p, ncol = n_add)
+      for(j in seq_along(which_add)) A[which_add[j],j] <- 1
+      myG <- cbind(myG, A)
     }
   }
+  myG <- myG*trait_scale
+  myG <- sumstatFactors:::norm_cols(myG)
   k <- ncol(myG)
   G <- rbind(c(1, rep(0, k)),
              cbind(rep(0, p), myG))

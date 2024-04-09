1bcb51727524a042f08460ce2257abfea18e042f
Stefan Eng <stefaneng13@gmail.com>
2024-04-09 08:44:49 -0400

    Move `rb` prefix to suffix for Ma winners curse

--- R/winners_curse_adj.R
+++ R/winners_curse_adj.R
@@ -4,13 +4,13 @@
 #'
 #' DOI: 10.1214/22-AOS2247
 #'
-#' @param beta Selected SNP effect size estimates
-#' @param se_beta Selected SNP effect size standard error
+#' @param beta Selected SNP effect size estimates (numeric or matrix)
+#' @param se_beta Selected SNP effect size standard error (numeric or matrix)
 #' @param eta a prespecified constant that reflects the noise level of the pseudo SNPs
 #' @param alpha significance level.
 #' @param lambda quantile of the standard normal distribution. If specified, overrides `alpha`.
 #'
-#' @return list with elements: rb_beta, rb_se if `est_se_beta` = TRUE and only a vector of Rao-Blackwellized estimates if `est_se_beta` = FALSE
+#' @return list with elements: beta_rb, se_rb if `est_se_beta` = TRUE and only a vector of Rao-Blackwellized estimates if `est_se_beta` = FALSE
 #' @export
 #'
 #' @examples
@@ -37,17 +37,17 @@ snp_beta_rb <- function(
     num <- dnorm(a_term_plus) - dnorm(a_term_neg)
     denom <- 1 - pnorm(a_term_plus) + pnorm(a_term_neg)
     ratio_term <- (num / denom)
-    rb_beta <- beta - (se_beta / eta) * ratio_term
+    beta_rb <- beta - (se_beta / eta) * ratio_term
 
     if (!est_se_beta) {
-      return(rb_beta)
+      return(beta_rb)
     } else {
       var_term1 <- ratio_term^2
       var_term2 <- (a_term_plus * dnorm(a_term_plus) -
                       a_term_neg * dnorm(a_term_neg)) / denom
       rb_var <- se_beta^2 * (1 + (var_term1 - var_term2) / eta^2)
 
-      return(list(rb_beta = rb_beta, rb_se = sqrt(rb_var)))
+      return(list(beta_rb = beta_rb, se_rb = sqrt(rb_var)))
     }
 }
 

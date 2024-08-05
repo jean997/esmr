aba9dbfd847649f17c63ca15862ede76247ec785
Stefan Eng <stefaneng13@gmail.com>
2024-08-05 09:41:47 -0400

    Document log_sum_exp and add gitignore

--- R/log_sum_exp.R
+++ R/log_sum_exp.R
@@ -1,3 +1,17 @@
+#' Compute log(exp(a) + exp(b) + ...) with more numerical stability
+#' @param x numeric vector or first element in the sum
+#' @param ... additional arguments to sum
+#' @return log(exp(a) + exp(b) + ...)
+#' @examples
+#' log_sum_exp(1, 2, 3)
+#' log_sum_exp(c(1, 2, 3))
+#' log_sum_exp(c(1,2), 3)
+#' # Too big to compute directly, exp(log_liks) = c(Inf, Inf, Inf)
+#' log_liks <- c(3000.5, 3001, 3000)
+#' log_denom <- log_sum_exp(log_liks)
+#' # Posterior probability with uniform prior on the three discrete states
+#' exp(log_liks - log_denom)
+#'
 log_sum_exp <- function(x, ...) {
   x <- unlist(c(x, list(...)))
   x_max <- max(x)

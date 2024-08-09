6a43caee96bb7ae19b87d6d44b91650f04979960
Stefan Eng <stefaneng13@gmail.com>
2024-08-09 10:14:37 -0400

    Fix issue with not updating parameters to current values

--- R/project_to_dag.R
+++ R/project_to_dag.R
@@ -1,13 +1,14 @@
 project_to_DAG <- function(
-    X, s = 1, lambda = c(10^-seq(1,5), 0), ix = NULL,
+    X, s = 1, lambda = c(1, 10^-seq(1,5), 0), ix = NULL,
     penalty = c("L1", "L2")) {
   penalty <- match.arg(penalty)
   d <- nrow(X)
   logdet_ix <- which(X != 0, arr.ind = TRUE)
   results <- list()
+  curr_pars <- X[logdet_ix]
   for (i in seq_along(lambda)) {
     results[[i]] <- optim(
-      par = X[logdet_ix],
+      par = curr_pars,
       fn = frob_logdet_vec,
       gr = frob_logdet_grad_vec,
       s = s,
@@ -17,12 +18,15 @@ project_to_DAG <- function(
       penalty = penalty,
       lambda = lambda[i],
       method = "BFGS",
-      control = list(maxit = 2000)
+      control = list(maxit = 2000, trace = 0)
     )
 
     if (results[[i]]$convergence != 0) {
       warning("Optimization did not converge at lambda: ", lambda[i])
     }
+
+    curr_pars <- results[[i]]$par
+    print(curr_pars)
   }
 
   X_dag <- matrix(0, ncol = d, nrow = d)

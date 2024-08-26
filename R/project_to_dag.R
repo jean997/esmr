4d57f6ca7ab7caf5107e800ee75f221651d43dc7
Stefan Eng <stefaneng13@gmail.com>
2024-08-26 08:55:15 -0400

    Add zap to DAG option to project_to_DAG

--- R/project_to_dag.R
+++ R/project_to_dag.R
@@ -1,6 +1,11 @@
+#' @export
 project_to_DAG <- function(
     X, s = 1, lambda = c(1, 10^-seq(1,5), 0), ix = NULL,
-    penalty = c("L1", "L2")) {
+    penalty = c("L2", "L1"),
+    zap_to_DAG = FALSE,
+    min_zap = 3,
+    start_zap = 8
+    ) {
   penalty <- match.arg(penalty)
   d <- nrow(X)
   logdet_ix <- which(X != 0, arr.ind = TRUE)
@@ -30,5 +35,19 @@ project_to_DAG <- function(
 
   X_dag <- matrix(0, ncol = d, nrow = d)
   X_dag[logdet_ix] <- results[[i]]$par
+
+  if (zap_to_DAG) {
+    # TODO: Replace this with a faster method?
+    is_dag <- esmr:::is_dag_matrix(X_dag)
+    #start_zap <- 7
+    while (! is_dag && start_zap >= min_zap) {
+      X_dag <-  zapsmall(X_dag, digits = start_zap)
+      start_zap <- start_zap - 1
+      is_dag <- esmr:::is_dag_matrix(X_dag)
+    }
+    if (start_zap < min_zap && ! is_dag) {
+      stop("projection could not be zapped to DAG with min_zap: ", min_zap, "digits")
+    }
+  }
   X_dag
 }

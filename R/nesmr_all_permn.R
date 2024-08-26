e461da62344aeaa2167934494709cef0c741417f
Stefan Eng <stefaneng13@gmail.com>
2024-08-26 08:55:58 -0400

    Add return model option for nesmr_all_permn

--- R/nesmr_all_permn.R
+++ R/nesmr_all_permn.R
@@ -1,19 +1,21 @@
+#' @export
 nesmr_all_permn <- function(
     beta_hat,
     se_beta_hat,
     B_templates = NULL,
     posterior_probs = TRUE,
+    return_model = FALSE,
     ...
-    ) {
+) {
   if (is.null(B_templates)) {
     d <- ncol(beta_hat)
     B_lower <- matrix(0, nrow = d, ncol = d)
     B_lower[lower.tri(B_lower)] <- 1
     if (d >= 9) {
       warning(
-      sprintf(
-        "With %d traits there are %d! = %d permutations. This will be slow...consider supplying only the permutations with parameter `B_templates`",
-        d, d, factorial(d))
+        sprintf(
+          "With %d traits there are %d! = %d permutations. This will be slow...consider supplying only the permutations with parameter `B_templates`",
+          d, d, factorial(d))
       )
     }
     all_perms <- combinat::permn(seq_len(d))
@@ -29,23 +31,29 @@ nesmr_all_permn <- function(
       G = diag(d),
       direct_effect_template = B,
       ...
-      )
+    )
     res$log_lik <- log_py(res)
     res
   })
 
+  rtn <- list()
+
+  if (return_model) {
+    rtn$nesmr_models <- nesmr_models
+  } else {
+    rtn$direct_effects <- lapply(nesmr_models, function(x) x$direct_effects)
+    rtn$se_dm <- lapply(nesmr_models, function(x) x$se_dm)
+    rtn$pvals_dm <- lapply(nesmr_models, function(x) x$pvals_dm)
+    rtn$B_template <- lapply(nesmr_models, function(x) x$direct_effects)
+    rtn$log_lik <- sapply(nesmr_models, function(x) x$log_lik)
+  }
+
   if (posterior_probs) {
-    # This is basically just softmax
     mod_log_lik <- sapply(nesmr_models, function(x) x$log_lik)
     log_denom <- log_sum_exp(mod_log_lik)
     posterior_probs <- exp(mod_log_lik - log_denom)
-    return(
-      list(
-        nesmr_models = nesmr_models,
-        posterior_probs = posterior_probs
-      )
-    )
+    rtn$posterior_probs <- posterior_probs
   }
 
-  return(nesmr_models)
+  return(rtn)
 }

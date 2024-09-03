6ac6685cdb500082fc99fe180567a3b3fe59d18a
Stefan Eng <stefaneng13@gmail.com>
2024-09-03 08:32:30 -0400

    Add tryCatch to better check if matrix is dag

--- R/is_dag.R
+++ R/is_dag.R
@@ -1,10 +1,14 @@
 # Checks if a matrix is a DAG
 # Uses Proposition 1 from arXiv:1803.01422v2
-is_dag_matrix <-function(x) {
+is_dag_matrix <- function(x) {
   d <- ncol(x)
   stopifnot(nrow(x) == d)
-  all(max(abs(eigen(x, only.values = TRUE)$values)) < 1) &&
-    sum(diag(solve(diag(d) - x))) == d
+  tryCatch({
+    all(max(abs(eigen(x, only.values = TRUE)$values)) < 1) &&
+      sum(diag(solve(diag(d) - x))) == d
+  }, error = function(e) {
+    FALSE
+  })
 }
 
 # Faster to topological sort than to check for cycles

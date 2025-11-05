d70796cbec65ee19f2feb16f2bf2792548bdd5b2
Jean Morrison <morrison.jeanv@gmail.com>
2025-11-05 18:51:33 -0500

    update docs

--- R/wrappers.R
+++ R/wrappers.R
@@ -8,7 +8,7 @@
 #'@param pval_thresh p-value threshold for variant selection (optional).
 #'@param variant_ix Indices of variants to include (optional).
 #'@param ld_scores List of ld_scores. If missing, no additional variance term will be added.
-#'@param RE
+#'@param RE RE
 #'@param max_iter Maximum number of iterations.
 #'@param params Additional parameters. See mvmr_default_params() for more information. Generally these can be left at their defaults.
 #'@export
@@ -38,21 +38,19 @@ esmr <- function(beta_hat_X, se_X,
 }
 
 #'@title Network Empirical Shrinkage MR
-#'@param beta_hat_Y Vector of SNP-outcome associations (length n)
-#'@param se_Y Standard errors of beta_hat_Y
-#'@param beta_hat_X Vector of SNP-exposure associations for the primary exposure (length n)
-#'@param se_X Vector of standard errors of beta_hat_X
-#'@param direct_effect_template
+#'@param beta_hat_X Matrix of SNP-trait associations (n by p)
+#'@param se_X Matrix of standard errors of beta_hat_X
+#'@param direct_effect_template p by p matrix with all elements equal to 0 or 1 specifying the DAG structure.
 #'@param R Nuisance correlation matrix if there is sample overlap (optional).
 #'@param pval_thresh p-value threshold for variant selection (optional).
 #'@param variant_ix Indices of variants to include (optional).
 #'@param ld_scores List of ld_scores. If missing, no additional variance term will be added.
-#'@param RE
+#'@param RE RE
 #'@param max_iter Maximum number of iterations.
 #'@param params Additional parameters. See nesmr_default_params() for more information. Generally these can be left at their defaults.
 #'@export
 nesmr <- function(beta_hat_X, se_X,
-                  direct_effect_template = NULL,
+                  direct_effect_template,
                   R = NULL,
                   pval_thresh = NULL,
                   variant_ix = NULL,
@@ -75,16 +73,21 @@ nesmr <- function(beta_hat_X, se_X,
 }
 
 #'@title Empirical Shrinkage Multivariable MR with Factors
-#'@param beta_hat_X Matrix of SNP-exposure associations (n by p)
-#'@param se_X matrix of standard errors of beta_hat_X
-#'@param direct_effect_template
+#'@param beta_hat_Y Vector of SNP-outcome associations (length n)
+#'@param se_Y Standard errors of beta_hat_Y
+#'@param beta_hat_X Vector of SNP-exposure associations (length n)
+#'@param se_X Standard errors of beta_hat_X
+#'@param beta_hat_Z Matrix of SNP-confounder associations (n by p-2).
+#'@param se_Z matrix of standard errors of beta_hat_Z (n by p-2).
+#'@param factors_matrix Factors matrix (p-2 by k).
+#'@param factors_residual_sd Factors residual sd
 #'@param R Nuisance correlation matrix if there is sample overlap (optional).
 #'@param pval_thresh p-value threshold for variant selection (optional).
 #'@param variant_ix Indices of variants to include (optional).
 #'@param ld_scores List of ld_scores. If missing, no additional variance term will be added.
-#'@param RE
+#'@param RE RE
 #'@param max_iter Maximum number of iterations.
-#'@param params Additional parameters. See nesmr_default_params() for more information. Generally these can be left at their defaults.
+#'@param params Additional parameters. See factor_default_params() for more information. Generally these can be left at their defaults.
 #'@export
 esmr_factors <- function(beta_hat_X, se_X,
                          beta_hat_Y, se_Y,

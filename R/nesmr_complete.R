5574be3ede73109bc27ccb773dec0ee6455df2ee
Stefan Eng <stefaneng13@gmail.com>
2025-11-17 19:19:16 -0500

    Add lower_tri option to nesmr_complete for n-mvmr steps

--- R/nesmr_complete.R
+++ R/nesmr_complete.R
@@ -5,6 +5,7 @@ nesmr_complete_mvmr <- function(
     beta_hat, se_beta_hat,
     pval_select = NULL,
     alpha = 5e-8,
+    lower_tri = FALSE,
     ...
   ) {
   stopifnot(all(dim(beta_hat) == dim(se_beta_hat)))
@@ -17,23 +18,31 @@ nesmr_complete_mvmr <- function(
     pval_select <- pval_cursed
   }
 
-  MVMR_models <- lapply(seq_len(d), function(i) {
-    mvmr_minp <- apply(pval_select[,-i, drop = FALSE], 1, min)
+  MVMR_models <- lapply(seq_len(d - 1), function(i) {
+    x_idx <- if (lower_tri) {
+      which(seq_len(d) > i)
+    } else {
+      seq_len(d)[-i]
+    }
+
+    mvmr_minp <- apply(pval_select[,x_idx, drop = FALSE], 1, min)
     mvmr_ix <- which(mvmr_minp < alpha)
 
     # Estimate G at each step for fair comparison
     tryCatch({
       esmr(beta_hat_Y = beta_hat[,i],
                     se_Y = se_beta_hat[,i],
-                    beta_hat_X = beta_hat[,-i],
-                    se_X = se_beta_hat[,-i],
+                    beta_hat_X = beta_hat[,x_idx],
+                    se_X = se_beta_hat[,x_idx],
                     variant_ix = mvmr_ix,
                     ...)
       }, error = function(e) {
         warning(e)
         list(beta = data.frame(
-          beta_m = rep(0, d - 1),
-          beta_s = rep(0, d - 1)
+          beta_m = rep(0, length(x_idx)),
+          beta_s = rep(0, length(x_idx)),
+          beta_j = rep(1, length(x_idx)),
+          beta_k = seq_along(x_idx)
         ))
       })
   })
@@ -42,12 +51,20 @@ nesmr_complete_mvmr <- function(
   # Create matrix from the effects
   mvmr_beta_df <- do.call(
     'rbind.data.frame',
-    lapply(1:d, function(i) {
+    lapply(seq_along(MVMR_models), function(i) {
       x <- MVMR_models[[i]]
 
+      x_idx <- if (lower_tri) {
+        which(seq_len(d) > i)
+      } else {
+        seq_len(d)[-i]
+      }
+
       res <- x$beta[c('beta_m', 'beta_s')]
-      res$to <- rep(i, d - 1)
-      res$from <- setdiff(1:d, i)
+      beta_to <- as.numeric(x$beta$beta_j)
+      beta_from <- as.numeric(x$beta$beta_k)
+      res$to <- rep(i, length(beta_to))
+      res$from <- c(i, x_idx)[beta_from]
       res
     })
   )
